5d36908f99a999ba866ade867c895ccf459b5cf9
chrisIyer <hellohotbox420@gmail.com>
2024-10-10 18:44:34 -0400

    scraping accomplished

--- index.js
+++ index.js
@@ -1,63 +1,23 @@
-import puppeteer from "puppeteer";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
-// TODO: Now it's your turn to improve the scraper and make him get more data from the Quotes to Scrape website.
-// Here's a list of potential improvements you can make:
-// - Navigate between all pages using the "Next" button and fetch the quotes on all the pages
-// - Fetch the quote's tags (each quote has a list of tags)
-// - Scrape the author's about page (by clicking on the author's name on each quote)
-// - Categorize the quotes by tags or authors (it's not 100% related to the scraping itself, but that can be a good improvement)
+// Fetch and scrape the web page
+async function scrapePage() {
+    try {
+        const response = await axios.get('https://quotes.toscrape.com/');
 
-const getQuotes = async () => {
-  // Start a Puppeteer session with:
-  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
+        // Load the HTML into cheerio
+        const $ = cheerio.load(response.data);
 
-  // Open a new page
-  const page = await browser.newPage();
+        // Extract data using Cheerio (like jQuery)
+        let quotes = $('.text').map((i, el) => $(el).text()).get(); // Fixed selector for text
+        let authors = $('.author').map((i, el) => $(el).text()).get(); // Fixed selector for authors
 
-  // On this new page:
-  // - open the "http://quotes.toscrape.com/" website
-  // - wait until the dom content is loaded (HTML is ready)
-  await page.goto("http://quotes.toscrape.com/", {
-    waitUntil: "domcontentloaded",
-  });
+        console.log('Quotes:', quotes);
+        console.log('Authors:', authors);
+    } catch (error) {
+        console.error('Error fetching the page:', error);
+    }
+}
 
-  // Get page data
-  const quotes = await page.evaluate(() => {
-    // Fetch the first element with class "quote"
-    // Get the displayed text and returns it
-    const quoteList = document.querySelectorAll(".quote");
-
-    // Convert the quoteList to an iterable array
-    // For each quote fetch the text and author
-    return Array.from(quoteList).map((quote) => {
-      // Get the sub-elements from the previously fetched quote element
-      const text = quote.querySelector(".text").innerText;
-      const author = quote.querySelector(".author").innerText;
-
-      return { text, author };
-    });
-  });
-
-  // Display the quotes
-  console.log(quotes);
-
-  // Click on the "Next page" button
-  await page.click(".pager > .next > a");
-
-  // Close the browser
-  await browser.close();
-    // Display the quotes
-    console.log(quotes);
-
-    // Click on the "Next page" button
-    await page.click(".pager > .next > a");
-  
-};
-
-// Start the scraping
-getQuotes();
+scrapePage();
