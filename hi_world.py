122d1a8cb4a8e7a18850b04361c4f267e9c012ce
chrisIyer <hellohotbox420@gmail.com>
2024-10-18 14:19:51 -0400

    Explanation of code.

--- hi_world.py
+++ hi_world.py
@@ -1,1 +1,1 @@
-print("Hi Listening rn, will call later. \nBut you can certainly get started. \nOne of the forist rules is this one: \n'Stop and think!'!")
+print("This random py file does not serve any particular purpose, other \nthan reflects my first py file. And so many firsts...\nFirst time executing python files from my own VS. \nIt happens to be in the Zali's stuff \nfolder because she and I will both be learning python as part \nof our mentorship. \nHi Listening rn, will call later. \nBut you can certainly get started. \nOne of the forist rules is this one: \n'Stop and think!'!")
