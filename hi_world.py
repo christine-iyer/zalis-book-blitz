fcd61db86f576a41226539db5a8e0721b2004758
chrisIyer <hellohotbox420@gmail.com>
2024-10-21 14:01:03 -0400

    I added a randomize function and I had to look up once again how to install on python 3.  python3 -m pip install ...

--- hi_world.py
+++ hi_world.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import random
 letter_data = {}
 with open('quid_data.csv', mode='r') as file:
      reader = csv.DictReader(file)
@@ -15,3 +16,17 @@ df = pd.DataFrame.from_dict(letter_data, orient='index', columns=['Points', 'Qua
 
 # Print the DataFrame as a table
 print(df["Quantity"])
+
+#draw a random card
+letter_data[letter] = {'Points': points, 'Quantity': quantity}
+
+# Convert the dictionary into a list of tuples (letter, points, quantity)
+letter_list = [(letter, data['Points'], data['Quantity']) for letter, data in letter_data.items()]
+
+# Shuffle the list in place
+random.shuffle(letter_list)
+
+# Print the shuffled list of letter cards
+print("Shuffled letter cards:")
+for card in letter_list:
+    print(card)
